22ebe99ff538f0afe6f80c5944f584fb6d67b407
Fazyyyy <rolvwin@gmail.com>
2023-12-02 14:54:04 +0800

    Update

--- elementbattle.js
+++ elementbattle.js
@@ -1,27 +1,30 @@
 const gameContainer = document.querySelector(".container"),
-userResult = document.querySelector(".user_result img"),
-cpuResult = document.querySelector(".cpu_result img"),
-option = document.querySelector(".option"),
-optionImages = document.querySelectorAll(".option_image");
+    userResult = document.querySelector(".user_result img"),
+    cpuResult = document.querySelector(".cpu_result img"),
+    option = document.querySelector(".option"),
+    optionImages = document.querySelectorAll(".option_image");
+
+let playerScore = 0;
+let cpuScore = 0;
 
 optionImages.forEach((image, index) => {
-image.addEventListener("click", (e) => {
-    image.classList.add("active");
-    optionImages.forEach((image2, index2) => {
-        index !== index2 && image2.classList.remove("active");
-        
-        })
+    image.addEventListener("click", (e) => {
+        image.classList.add("active");
+        optionImages.forEach((image2, index2) => {
+            index !== index2 && image2.classList.remove("active");
+        });
+
         let userChoice = e.target.querySelector("img").src;
 
         if (userChoice.includes("fireenergy.png")) {
-            userResult.src = "Charizard.png"; 
-        } else if(userChoice.includes("waterenergy.png")){
+            userResult.src = "Charizard.png";
+        } else if (userChoice.includes("waterenergy.png")) {
             userResult.src = "Blastoise.png";
-        } else if(userChoice.includes("grassenergy.png")){
+        } else if (userChoice.includes("grassenergy.png")) {
             userResult.src = "Venusaur.png";
-        } else{
+        } else {
             userResult.src = userChoice;
-        };
+        }
 
         let cpuChoiceIndex = Math.floor(Math.random() * 3);
         let cpuChoices = [
@@ -31,15 +34,15 @@ image.addEventListener("click", (e) => {
         ];
         let cpuChoice = cpuChoices[cpuChoiceIndex];
         if (cpuChoice.includes("fireenergy.png")) {
-            cpuResult.src = "Charizard.png"; 
-        } else if(cpuChoice.includes("waterenergy.png")){
+            cpuResult.src = "Charizard.png";
+        } else if (cpuChoice.includes("waterenergy.png")) {
             cpuResult.src = "Blastoise.png";
-        } else if(cpuChoice.includes("grassenergy.png")){
+        } else if (cpuChoice.includes("grassenergy.png")) {
             cpuResult.src = "Venusaur.png";
-        } else{
+        } else {
             cpuResult.src = cpuChoice;
-        };
-        console.log(userResult, cpuResult);
+        }
+
         // Determine the winner;
         if (
             (userChoice.includes("grassenergy.png") && cpuChoice.includes("fireenergy.png")) ||
@@ -47,14 +50,35 @@ image.addEventListener("click", (e) => {
             (userChoice.includes("waterenergy.png") && cpuChoice.includes("grassenergy.png"))
         ) {
             option.textContent = "CPU wins!";
+            cpuScore++;
         } else if (
             (userChoice.includes("fireenergy.png") && cpuChoice.includes("grassenergy.png")) ||
             (userChoice.includes("waterenergy.png") && cpuChoice.includes("fireenergy.png")) ||
             (userChoice.includes("grassenergy.png") && cpuChoice.includes("waterenergy.png"))
         ) {
             option.textContent = "You win!";
+            playerScore++;
         } else {
             option.textContent = "It's a tie!";
         }
+
+
+        updateScores();
+    });
 });
+newGameBtn.addEventListener("click", () => {
+    
+    playerScore = 0;
+    cpuScore = 0;
+
+    
+    updateScores();
+
+    userResult.src = "pokeball.png";
+    cpuResult.src = "pokeball.png";
 });
+
+function updateScores() {
+    document.getElementById('playerScore').innerHTML = `${playerScore}`;
+    document.getElementById('cpuScore').innerHTML = `${cpuScore}`;
+}
